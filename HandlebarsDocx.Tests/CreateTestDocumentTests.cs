76d6acc63ab62b087a42bdcc0daab6717cb3f4ed
Andrew Stewart <Andrew.Stewart@advancedcomputersoftware.com>
2016-01-28 09:19:18 +0000

    Initial project outline

---
+++ HandlebarsDocx.Tests/CreateTestDocumentTests.cs
@@ -0,0 +1,33 @@
+using Xunit;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace HandlebarsDocx
+{
+    // see example explanation on xUnit.net website:
+    // https://xunit.github.io/docs/getting-started-dnx.html
+    public class CreateTestDocumentTests
+    {
+        [Fact]
+        public void TestDocumentContainsExpectedText()
+        {
+            using (var docs = CreateTestDocument("Hello World"))
+            {
+                Assert.Equal("Hello World", docs.MainDocumentPart.Document.Body.InnerText);
+            }
+        }
+
+        private static WordprocessingDocument CreateTestDocument(string contents)
+        {
+            var docs = WordprocessingDocument.Create("c:\\repos\\a.docx", WordprocessingDocumentType.Document);
+            var mainPart = docs.AddMainDocumentPart();
+            mainPart.Document = new Document();
+            var body = mainPart.Document.AppendChild(new Body());
+            var para = body.AppendChild(new Paragraph());
+            var run = para.AppendChild(new Run());
+            run.AppendChild(new Text(contents));
+            return docs;
+        }
+    }
+}
